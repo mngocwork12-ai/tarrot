13df112c34d85787e2680b6597230fd4956c6f9f
mngocwork12-ai <mngocwork12@gmail.com>
2026-04-20 16:19:24 +0700

    open cors for railway

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import { fileURLToPath } from "url";
 import { dirname } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, "public")));
 
